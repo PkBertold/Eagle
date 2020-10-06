5499ccdc983db6f08c8052a7f773e29586f78a28
Ladányi Bertold <bertold95pk@gmail.com>
2020-10-06 19:20:33 +0200

    chaos

--- kerter.py
+++ kerter.py
@@ -1,10 +1,10 @@
 #Elkezdhetünk ebbe dolgozni
-<<<<<<< HEAD
+
 def teglalapKerulet():
 	a=float(input("Kérem a téglalap egyik oldalát:"))
 	b=float(input("Kérem a téglalap másik oldalát:"))
 	return float(2*(a+b))
-=======
+
 def triangle(a,b,c):
     print("perimeter: ",a+b+c)
 triangle(3,4,5)
@@ -15,4 +15,20 @@ a = input("a oldal: ")
 b = input("b oldal: ")
 c = input("c oldal: ")
 triangle(a,b,c)
->>>>>>> 048e075ddf40ad4eaa83b87c88a1170b81169fac
+
+def nyolcszogKerulet():
+	a=float(input("Kérem a 8szög oldalát:"))
+	return float(8*a)
+def nyolcszogTerulet():
+	a=float(input("Kérem a 8szög oldalát")
+	r=float(input("Kérem adja meg a 8szög sugarát:"))
+	return float (4*a*r)
+	
+print("1 - Háromszög")
+print("2 - Kör")
+print("3 - Téglalap")
+print("4 - Nyolcszög")
+v=input("Milyen alakztattal szeretnél dolgozni?")
+if v=="3":
+	teglalapKerulet())
+	teglalapTerulet())
