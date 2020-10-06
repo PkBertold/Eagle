048e075ddf40ad4eaa83b87c88a1170b81169fac
Szoftver <sisina.akemi@gmail.com>
2020-10-06 19:02:37 +0200

    nah

--- kerter.py
+++ kerter.py
@@ -1,1 +1,11 @@
 #Elkezdhetünk ebbe dolgozni
+def triangle(a,b,c):
+    print("perimeter: ",a+b+c)
+triangle(3,4,5)
+
+def triangle(a,b,c):
+    print("ker: ",a+b+c)
+a = input("a oldal: ")
+b = input("b oldal: ")
+c = input("c oldal: ")
+triangle(a,b,c)
