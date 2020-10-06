cde90fb8ff30ad319129b8deb27bfcaa5a86dd33
Sipos Péter <petex0818@gmail.com>
2020-10-06 19:13:25 +0200

    téglalap kerület, terület

--- kerter.py
+++ kerter.py
@@ -1,1 +1,11 @@
 #Elkezdhetünk ebbe dolgozni
+def TeglalapKerulet():
+    a=float(input("kérem adja meg a téglalap a oldalát(kerület)[cm]:"))
+    b=float(input("kérem adja meg a téglalap b oldalát(kerület)[cm]:"))
+    return float(2*(a+b))
+def TeglalapTerulet():
+    a=float(input("kérem adja meg a téglalap a oldalát(terület)[cm]:"))
+    b=float(input("kérem adja meg a téglalap a oldalát(terület)[cm]:"))
+    return float(a*b)
+print (TeglalapTerulet())
+print (TeglalapKerulet())  
